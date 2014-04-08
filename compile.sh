5a91495cef9b9ead769fc741112c637773ec4ca3
L. Gabriel Miller <gmiller2007@gmail.com>
2014-04-07 20:55:23 -0400

    Removes the entire _site directory prior to compilation.

--- compile.sh
+++ compile.sh
@@ -4,14 +4,14 @@ static=$cwd/jekyll/_static
 
 compass compile -c scss/config.rb
 
-rm -r $www/*
+rm -r $www && mkdir $www
 
 cd jekyll && jekyll build
 
+#This is the poor man's modrewrite
 for file in $(find $www -type f)
 do
     if [ -f $file ] ; then
-        # name without extension
         name=${file%\.*}
         mv ${file} ${name}
     fi ;
